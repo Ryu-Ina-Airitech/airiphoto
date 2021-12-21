ec13c3d3bd53e3dd95b6eb238c1c3f1b29cbea4a
EiEiMon-airitech <ei.ei.mon@airitech.co.jp>
2021-12-21 20:12:37 +0900

    Commit BackendConfigCommonFile_20211221

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,28 +1,91 @@
 export type AmplifyDependentResourcesAttributes = {
-  function: {
-    sampleapi: {
-      Name: "string";
-      Arn: "string";
-      Region: "string";
-      LambdaExecutionRole: "string";
-    };
-  };
-  api: {
-    sampleapi: {
-      RootUrl: "string";
-      ApiName: "string";
-      ApiId: "string";
-    };
-  };
-  auth: {
-    airiphotofb50055a: {
-      IdentityPoolId: "string";
-      IdentityPoolName: "string";
-      UserPoolId: "string";
-      UserPoolArn: "string";
-      UserPoolName: "string";
-      AppClientIDWeb: "string";
-      AppClientID: "string";
-    };
-  };
+    "function": {
+        sampleapi: {
+        Name: "string";
+        Arn: "string";
+        Region: "string";
+        LambdaExecutionRole: "string";
+      },
+        "UserOperationApiFunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "ImageOperationApiFunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "ImageTagsNameOperationApiFunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "ImageInfoTagOperationApiFunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        ampleapi: {
+            RootUrl: "string";
+            ApiName: "string";
+            ApiId: "string";
+          },
+        "photostockapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "storage": {
+        "UserStorage": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        },
+        "ImageStorage": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        },
+        "ImageInfoTagsStorage": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        },
+        "TagsStorage": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    },
+    auth: {
+        airiphotofb50055a: {
+          IdentityPoolId: "string";
+          IdentityPoolName: "string";
+          UserPoolId: "string";
+          UserPoolArn: "string";
+          UserPoolName: "string";
+          AppClientIDWeb: "string";
+          AppClientID: "string";
+        };
+      };
 };
