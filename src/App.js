fa729d515e5001ee82b18e4763c29bec723ef307
yutezuka <yu.tezuka@airitech.co.jp>
2021-12-27 16:37:49 +0900

    update Interface

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Amplify from "aws-amplify";
 import { Home } from "./Home";
 import { Login } from "./Login";
 import "./styles.css";
-import { Navigate, Outlet } from "react-router-dom";
+// import { Navigate, Outlet } from "react-router-dom";
 import awsExports from "./aws-exports";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UploadView } from "./uploadView";
 
 Amplify.configure(awsExports);
 
-const PrivateRoute = () => {
-  const auth = useAuthenticator(); // determine if authorized, from context or however you're doing it
+// const PrivateRoute = () => {
+//   const auth = useAuthenticator(); // determine if authorized, from context or however you're doing it
 
-  // If authorized, return an outlet that will render child elements
-  // If not, return element that will navigate to login page
-  return auth ? <Outlet /> : <Navigate to="/" />;
-};
+//   // If authorized, return an outlet that will render child elements
+//   // If not, return element that will navigate to login page
+//   return auth ? <Outlet /> : <Navigate to="/" />;
+// };
 
 export default function App() {
   const { user } = useAuthenticator();
@@ -27,11 +27,12 @@ export default function App() {
     return (
       <BrowserRouter>
         <Fragment>
+          <Home />
           <Routes>
-            <Route path="/" element={<PrivateRoute />}>
-              <Route path="/Home" element={<Home />} />
-              <Route path="/UploadView" element={<UploadView />} />
-            </Route>
+            {/* <Route path="/" element={<PrivateRoute />}> */}
+            <Route path="/Home" element={<Home />} />
+            <Route path="/UploadView" element={<UploadView />} />
+            {/* </Route> */}
           </Routes>
         </Fragment>
       </BrowserRouter>
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Header } from "./Header";
 // import UploadView from "./uploadView";
 
 export function Home() {
@@ -7,6 +8,7 @@ export function Home() {
 
   return (
     <main>
+      <Header />
       <h1>Hello {user.attributes.email}</h1>
       <button onClick={signOut}>Sign out</button>
       <a href="/UploadView">UploadView</a>
